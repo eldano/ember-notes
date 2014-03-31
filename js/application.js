e126f582dbf73181246f8f081995a3a6198edcd2
eldano <eldano.v2@gmail.com>
2014-03-31 17:36:46 -0300

    remove reveal

--- js/application.js
+++ js/application.js
@@ -6,11 +6,11 @@ App.Store = DS.Store.extend({
 	adapter: DS.FixtureAdapter.create()
 });
 
-App.ApplicationView = Ember.View.extend({
-	initFoundation: function() {
-		return Em.$(document).foundation();
-	}.on('didInsertElement')
-});
+//App.ApplicationView = Ember.View.extend({
+//	initFoundation: function() {
+//		return Em.$(document).foundation();
+//	}.on('didInsertElement')
+//});
 
 Ember.View.reopen({
   init: function() {
