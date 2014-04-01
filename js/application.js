d48155d086447652900a08e0ae4828de00bce4d6
eldano <eldano.v2@gmail.com>
2014-04-01 17:34:57 -0300

    add mode for local vs network data

--- js/application.js
+++ js/application.js
@@ -2,11 +2,34 @@ window.App = Ember.Application.create();
 
 //App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
-App.Store = DS.Store.extend({
-	adapter: DS.FixtureAdapter.create()
-});
+App.mode = "local";//
 
-//App.ApplicationView = Ember.View.extend({
+if(App.mode == "local") {
+	window.App.Store = DS.Store.extend({
+		adapter: DS.FixtureAdapter.create()
+	});
+}
+else {
+
+	window.App.ApplicationSerializer = DS.RESTSerializer.extend({
+	  primaryKey: "_id"
+	});
+	window.App.ApplicationAdapter = DS.RESTAdapter.extend({
+	  host: 'http://192.168.1.107:4242',
+	  namespace: 'api',
+
+	  ajax: function(url, method, hash) {
+	    hash = hash || {}; // hash may be undefined
+	    hash.crossDomain = true;
+	    //hash.xhrFields = {withCredentials: true};
+	    return this._super(url, method, hash);
+	  }
+	  
+	});
+
+}
+
+//window.App.ApplicationView = Ember.View.extend({
 //	initFoundation: function() {
 //		return Em.$(document).foundation();
 //	}.on('didInsertElement')
